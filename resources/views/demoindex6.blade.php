99240dacdfa8024476048cb478d899bbb9093db4
Dong-Pham <phamdong5601@gmail.com>
2026-05-19 00:16:58 +0700

    Them DemoController route va views

---
+++ resources/views/demoindex6.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Demo Index 6</title>
+</head>
+
+<body>
+
+    <h1>Xin chào bạn đến với hệ thống {{ $data }}</h1>
+
+    <h2>
+        Value: {{ $param1 }} - {{ $param2 }}
+    </h2>
+
+</body>
+
+</html>
